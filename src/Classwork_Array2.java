4445ec2e5fae14b75c8c1517add73b07862d59bd
LasmaWhite <lasma.baltina@inbox.lv>
2021-07-25 19:06:05 +0300

    add

--- src/Classwork_Array2.java
+++ src/Classwork_Array2.java
@@ -1,6 +1,15 @@
 public class Classwork_Array2 {
     public static void main(String[] args) {
 
+//        Create a simple 1 dimensional array of 10 integers. Select the values yourself
+//        1) In a for loop print all odd values
+//        2)  Calculate and print sum of all odd values
+//
+//        BONUS
+//        find the smallest value in the array
+//        Find the largest element in the array
+
+
 
         int[] integerArray = {10,6,3,4,8,2,12,58,94,33};
 
